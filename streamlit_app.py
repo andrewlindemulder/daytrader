ed92a723ec869e0a93b9a2c62795add9cb272a14
andrewlindemulder <lindemulder.a@gmail.com>
2024-11-04 14:23:12 -0500

    corrected

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import requests
 
 with st.form("my_form"):
     ticker = st.text_input("Enter the stock ticker")
-    limit = st.number_input("Enter your age", step="4")
-    submit_button = st.form_submit_button("Submit")
+    limit = st.number_input("Enter your age", step=4)
+    submit_button = st.form_submit_button("Submit Order")
 
 if submit_button:
     st.write("Hello,", ticker, "you are", limit, "years old.")

caa3edc3a901e2c9775b5568e92be651f5435ed9
andrewlindemulder <lindemulder.a@gmail.com>
2024-11-04 14:21:43 -0500

    fixed limit...

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import requests
 
 with st.form("my_form"):
     ticker = st.text_input("Enter the stock ticker")
-    limit = st.number_input("Enter your age")
-    submit_button = st.form_submit_button("Submit", step="4")
+    limit = st.number_input("Enter your age", step="4")
+    submit_button = st.form_submit_button("Submit")
 
 if submit_button:
     st.write("Hello,", ticker, "you are", limit, "years old.")

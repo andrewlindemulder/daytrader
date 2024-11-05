520cc48fb67d668684cb2668d4a6de56ab35c082
andrewlindemulder <lindemulder.a@gmail.com>
2024-11-04 20:16:20 -0500

    test order

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import requests
 import math
 from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import LimitOrderRequest
+from alpaca.trading.enums import OrderSide, TimeInForce
 
 with st.form("my_form"):
     ticker = st.text_input("Enter the stock ticker")
@@ -17,10 +19,20 @@ if submit_button:
     else:
         apikey=st.secrets["alpaca_api_key"]
         apisecret=st.secrets["alpaca_api_secret"]
-        trading_client = TradingClient(apikey, apisecret, paper=False)
+        trading_client = TradingClient(apikey, apisecret, paper=True)
         account = trading_client.get_account()
 
         purchase_qty = math.floor((float(account.buying_power) / float(limit)) * 100)/100.0
 
+        limit_order_data = LimitOrderRequest(
+                    symbol="TSLA",
+                    limit_price=100,
+                    notional=200,
+                    side=OrderSide.BUY,
+                    time_in_force=TimeInForce.DAY
+                   )
 
-        st.write(purchase_qty)
+        limit_order = trading_client.submit_order(
+                order_data=limit_order_data
+              )
+        st.write(limit_order)

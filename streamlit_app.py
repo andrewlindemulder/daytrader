b86c6d6a3bad5eb08c405dd6c92b7525b14d42fd
andrewlindemulder <lindemulder.a@gmail.com>
2024-11-04 19:44:24 -0500

    get account

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import pandas as pd
 import requests
+from alpaca.trading.client import TradingClient
 
 with st.form("my_form"):
     ticker = st.text_input("Enter the stock ticker")
-    limit = st.number_input("Enter your age")
+    limit = st.number_input("Enter the buy price")
     submit_button = st.form_submit_button("Submit Order")
 
 if submit_button:
-    st.write("Hello,", ticker, "you are", limit, "years old.")
+    trading_client = TradingClient(st.secrets["alpaca_api_key"], st.secrets["alpaca_api_secret"])
+    account = trading_client.get_account()
+    st.write(account)
